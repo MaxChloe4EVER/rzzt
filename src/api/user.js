27df32aadd6594c51c4f8653f37e773dece51414
MaxChloe4EVER <g78529824@qq.com>
2021-07-24 14:21:26 +0800

    封装单独的登录请求接口

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,11 @@
-// import request from '@/utils/request'
+import request from '@/utils/request'
 
 export function login(data) {
-
+  request({
+    url: '/sys/login',
+    method: 'POST',
+    data
+  })
 }
 
 export function getInfo(token) {
