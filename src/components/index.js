31dbd265144da64e7e881a3f4123ed1982b4cc6b
MaxChloe4EVER <g78529824@qq.com>
2021-08-03 08:57:22 +0800

    员工导入组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,11 @@
 import PageTools from '@/components/PageTools'
+import UploadExcel from '@/components/UploadExcel'
 
 export default {
   install(Vue) {
+    // 注册工具栏组件
     Vue.component('PageTools', PageTools)
+    // 注册导入excel组件
+    Vue.component('UploadExcel', UploadExcel)
   }
 }
